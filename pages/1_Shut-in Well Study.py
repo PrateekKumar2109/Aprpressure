d574ab7c720096902c4bb2dc920b07a2d870b9ec
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-11 15:14:31 +0530

    Update 1_Shut-in Well Study.py

--- pages/1_Shut-in Well Study.py
+++ pages/1_Shut-in Well Study.py
@@ -220,7 +220,7 @@ def pressure_plot_down(well_name,dataframe):
                  xytext=(-100,0), # distance from text to points (x,y)
                  ha='left',fontsize=6) # horizontal alignment can be left, right or center
     plt.annotate(label_o, # this is the text
-                 (xs-200,ys-400), # these are the coordinates to position the label
+                 (xs-400,ys-400), # these are the coordinates to position the label
                  textcoords="offset points", # how to position the text
                  xytext=(-100,0), # distance from text to points (x,y)
                  ha='left',fontsize=6)
