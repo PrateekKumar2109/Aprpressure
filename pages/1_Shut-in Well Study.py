544dc2c7803069abe717303ad86c4ae64023ac95
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-11 14:41:14 +0530

    Update 1_Shut-in Well Study.py

--- pages/1_Shut-in Well Study.py
+++ pages/1_Shut-in Well Study.py
@@ -193,7 +193,7 @@ def pressure_temp_plot(well_name,dataframe):
 
 def pressure_plot_down(well_name,dataframe):
     colors = {'oil':'green', 'gas':'red', 'water':'blue'}
-    fig=plt.figure(figsize=(3.6,4),dpi=30)
+    fig=plt.figure(figsize=(2,3),dpi=30)
     plt.plot(dataframe['PRESSURE'],dataframe['TVDSS'],color='black',lw=1,label='Pressure')
     #plt.scatter(df_final['PRESSURE'],df_final['TVDSS'],marker='o',c=df_final['Fluid type'].map(colors))
     groups = df_final.groupby('Fluid type')
