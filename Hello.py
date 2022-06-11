7cca327d978acf54ebb7b74ec81894404f2c50d9
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-11 06:55:34 +0530

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Pressure Made Esay! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    We are here to make your life easy by analyzing pressures and temperatures gradients
+    
+"""
+)
