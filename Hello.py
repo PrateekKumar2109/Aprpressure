58bb2675a06e5febef45dd681c39dda75f74d626
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-11 17:38:55 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -32,7 +32,7 @@ if choice=='Login':
         else:
             st.warning('Wrong Password')
     else:
-else:   
+ else:   
     st.write("""With the Release Streamlit Version 1.10.0 it is now possible to make a Multi-Page application 
      eliminating need of third party plugins. In this Web application we are working on Pressure & Temperature Survey Data
      which is used to make decision in day to day life of a well. We 
