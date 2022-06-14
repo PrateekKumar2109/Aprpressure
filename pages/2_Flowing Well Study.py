87579fe01088540c636a91bc64d908accfd6c29b
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-14 21:25:30 +0530

    Update 2_Flowing Well Study.py

--- pages/2_Flowing Well Study.py
+++ pages/2_Flowing Well Study.py
@@ -95,7 +95,7 @@ def flwing_press_temp_plt(wellnam,df_final_list,y_c,ang_point,gas_grad,gip,choic
     m=[0,0,0,0]
     headings=df_final['VALVES'].values
     
-    fig=plt.figure(figsize=(12,16),dpi=70)
+    fig=plt.figure(figsize=(16,18),dpi=70)
     ax = fig.add_subplot(211)
 
     ax.set_title(wellnam+' FBHP Pressure with Depth Plot ',fontsize=20)
@@ -155,7 +155,7 @@ def flwing_press_temp_plt(wellnam,df_final_list,y_c,ang_point,gas_grad,gip,choic
 #plt.gca().invert_yaxis()
 #ax2.invert_yaxis()
     ax.tick_params(axis='x',which='minor',direction='out',bottom=True,length=5)
-    ax.legend(loc='upper right', fontsize='x-large',bbox_to_anchor=(0.99, 0.94))
+    ax.legend(loc='upper right', fontsize='x-large',bbox_to_anchor=(0.99, 0.98))
     ax2.legend(loc='upper right', fontsize='x-large',bbox_to_anchor=(0.99, 0.84))
     ax2.tick_params( axis='y',labelsize=14,direction='out', length=6, width=2, colors='black',
                grid_color='r', grid_alpha=0.5)
