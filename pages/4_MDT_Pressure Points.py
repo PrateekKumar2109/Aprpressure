432e805f7713450cbc5a67afdc07056db07c5de0
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2022-06-24 17:00:22 +0530

    Update 4_MDT_Pressure Points.py

--- pages/4_MDT_Pressure Points.py
+++ pages/4_MDT_Pressure Points.py
@@ -7,7 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
-import scipy.spatial 
+import scipy
+from scipy import spatial
 
 st.title("""MDT Presssure Points analysis  """)
 
